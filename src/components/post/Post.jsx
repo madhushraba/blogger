09f633bc1399c41e9caff088f5c61106712da485
Madhushraba Das <madhushraba21das@gmail.com>
2022-04-01 04:33:24 +0530

    committed  via git desktop

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./post.css";
 
-export default function Post({img}) {
+export default function Post({ img }) {
   return (
     <div className="post">
       <img
         className="postImg"
-        src={img}
-
-         src="https://images.pexels.com/photos/1122637/pexels-photo-1122637.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-         alt=""
+        // src={img}
+        src="https://images.pexels.com/photos/1122637/pexels-photo-1122637.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        alt=""
       />
       <div className="postInfo">
         <div className="postCats">
@@ -18,7 +17,6 @@ export default function Post({img}) {
             <Link className="link" to="/posts?cat=Music">
               Music
             </Link>
-           
           </span>
           <span className="postCat">
             <Link className="link" to="/posts?cat=Music">
@@ -28,7 +26,7 @@ export default function Post({img}) {
         </div>
         <span className="postTitle">
           <Link to="/post/abc" className="link">
-            Lorem ipsum dolor sit 
+            Lorem ipsum dolor sit
           </Link>
         </span>
         <hr />
@@ -43,10 +41,3 @@ export default function Post({img}) {
     </div>
   );
 }
-
-
-
-
-
-
-
